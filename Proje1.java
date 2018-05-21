64e3899ac655d20f758cb45b80d88a29504b4e2b
TanserBey <39486803+TanserBey@users.noreply.github.com>
2018-05-21 14:48:36 +0300

    Update and rename README.md to Proje1.java

---
+++ Proje1.java
@@ -0,0 +1,8 @@
+int x = 10;
+int y = 10;
+System.out.println("10 ile 10'un Toplamı");
+System.out.println("Toplamı istiyorsanız (y) istemiyorsanız (n) ");
+Scanner işlem = new Scanner (System.in);
+int Yapılacakİşlem = işlem.nextInt();
+System.out.println(x+y);
+Scanner naber = new Scanner (System.in);
